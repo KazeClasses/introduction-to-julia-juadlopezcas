de45f5e8c98994d68767d4d4cab89ae77f87300d
daniellopez <daniel.lopezcastano@gmail.com>
2024-09-19 20:24:53 -0400

    last push

---
+++ docs/make.jl
@@ -0,0 +1,2 @@
+using Documenter, InsertionSort
+makedocs(sitename="Insertion Sort")
--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,20 @@
 using Test
 using Random
 
+"""
+Insertion sort algorithm.
+
+This function sorts an array using the insertion sort algorithm.
+
+# Arguments
+- `arr::Vector{Int}`: The array to be sorted.
+
+# Examples
+julia
+arr = [3, 2, 1]
+insertion_sort!(arr)
+println(arr) [1, 2, 3]
+"""
 function insertion_sort!(array::Array{Int, 1})
     # Insert your code here
     for i in 2:length(array)
